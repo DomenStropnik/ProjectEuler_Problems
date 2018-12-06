a0a5974c78aadbde27a8f4572f4c814fdcfc1231
DomenStropnik <stropnik.domen@outlook.com>
2018-12-06 20:52:32 +0100

    update

--- euler015.cpp
+++ euler015.cpp
@@ -7,6 +7,7 @@
  *
  */
 
+/*
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -60,4 +61,21 @@ int nFactorial2(int x)
     times *= nVector[i];
 
   return times;
+}
+*/
+
+#include <iostream>
+
+int main()
+{
+  const int gridSize = 20;
+  long long paths = 1;
+
+  for (int i = 0; i < gridSize; i++) {
+    paths *= (2 * gridSize) - i;
+    paths /= i + 1;
+  }
+
+  std::cout << paths << std::endl;
+  return 0;
 }
